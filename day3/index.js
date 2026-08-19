1c9aa84ecc3d79a83e78bb106179064f545417d8
VISHAL KUMAR JHA <kjvishal336@gmail.com>
2026-08-19 13:35:25 +0530

    day3

--- day3/index.js
+++ day3/index.js
@@ -1,6 +1,6 @@
 let A=[3,5,6,7,8,9,10];
 let B=[1,2,3,4,5,6,7,8,9,10];
-let k="Hello World";
+let k="Hello Woreld";
 console.log(k.length);
 console.log(A);
 console.log(A.length);
@@ -16,5 +16,8 @@ let g=k.toLowerCase();
 console.log(g);
 
 console.log(k.charAt(0));
-console.log(k.indexOf("World"));
+console.log(k.indexOf("Woreld"));
 console.log(k.lastIndexOf("e"));
+
+let m=k.slice(0,5);
+console.log(m);
